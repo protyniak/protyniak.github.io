11f270c14d1fb382ed169b28a9200bd401fd99e0
protyniak <oleksii.protyniak@gmail.com>
2020-10-21 13:40:40 +0300

    v.0.0.2

--- vikings/js/main.js
+++ vikings/js/main.js
@@ -5,4 +5,18 @@ $(function () {
             showinfo: 0
         },
     });
+    $('.heroes__slider-img').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        asNavFor: '.heroes__slider-text',
+        prevArrow: '<button class="slick-btn slick-prev"><img src="images/left-str.png" alt="prev"></button>',
+        nextArrow: '<button class="slick-btn slick-next"><img src="images/right-str.png" alt="next"></button>'
+    });
+    $('.heroes__slider-text').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        asNavFor: '.heroes__slider-img',
+        fade: true,
+        arrows: false
+    });
 });
