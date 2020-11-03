2287b006d28809969b63c0bd84fdab1421060f22
Oleksii Protyniak <59565883+protyniak@users.noreply.github.com>
2020-11-03 14:18:10 +0200

    first page/animation/slider

--- serfing/app/js/main.js
+++ serfing/app/js/main.js
@@ -3,7 +3,14 @@ $(function(){
 $('.header__slider').slick({
     infinity: true,
     fade: true,
+    prevArrow: '<img class="slider-arrows slider-arrows__left" src="images/arroes-left.svg" alt="right">',
+    nextArrow: '<img class="slider-arrows slider-arrows__right" src="images/arroes-right.svg" alt="right">',
+    asNavFor: ".slider-dots",
 });
-
+$('.slider-dots').slick({
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    asNavFor: ".header__slider",
+});
   
 });
